4831e547e9ac1faec39cb14f0ad75117b22db9ab
khw00185 <ksng0185@naver.com>
2025-02-14 21:54:40 +0900

    feat:유저 CRUD 및 JWT 인증인가 구현

--- Newsfeed.sql
+++ Newsfeed.sql
@@ -1,3 +1,5 @@
+USE newsfeed;
+-- Users 테이블 생성
 CREATE TABLE users (
                        id VARCHAR(26) NOT NULL PRIMARY KEY,
                        email VARCHAR(255) NOT NULL UNIQUE,
@@ -10,10 +12,12 @@ CREATE TABLE users (
                        is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
                        created_at DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
                        modified_at DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- RefreshToken 테이블 생성
 CREATE TABLE refresh_token (
                                id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                               users_id VARCHAR(26) NOT NULL UNIQUE,
                                refresh_token VARCHAR(255) NOT NULL,
-                               member_id VARCHAR(26) UNIQUE,
-                               FOREIGN KEY (member_id) REFERENCES users(id) ON DELETE CASCADE
-);
+                               FOREIGN KEY (users_id) REFERENCES users(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
