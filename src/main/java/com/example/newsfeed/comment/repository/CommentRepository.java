3ebc50e4a109978729d459ee1c504ec5ae2ed66b
myeonghoon <mhoo999@naver.com>
2025-02-18 18:23:25 +0900

    안 쓰는 메소드 삭제

--- src/main/java/com/example/newsfeed/comment/repository/CommentRepository.java
+++ src/main/java/com/example/newsfeed/comment/repository/CommentRepository.java
@@ -1,19 +1,10 @@
 package com.example.newsfeed.comment.repository;
 
-import com.example.newsfeed.comment.dto.CommentCountDto;
 import com.example.newsfeed.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-    @Query("SELECT NEW com.example.newsfeed.comment.dto.CommentCountDto(c.board.id, count(c)) " +
-            "FROM Comment c " +
-            "WHERE c.board.id in :boardIds " +
-            "GROUP BY c.board.id")
-    List<CommentCountDto> countByBoardIds(List<Long> boardIds);
-
     List<Comment> findByBoardId(String boardId);
 }
