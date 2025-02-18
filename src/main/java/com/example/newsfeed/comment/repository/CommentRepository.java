8b6ae3baff8e093b5e2cd60241dac013f67497a0
myeonghoon <mhoo999@naver.com>
2025-02-18 18:13:26 +0900

    comment 기본 crud 추가

---
+++ src/main/java/com/example/newsfeed/comment/repository/CommentRepository.java
@@ -0,0 +1,19 @@
+package com.example.newsfeed.comment.repository;
+
+import com.example.newsfeed.comment.dto.CommentCountDto;
+import com.example.newsfeed.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query("SELECT NEW com.example.newsfeed.comment.dto.CommentCountDto(c.board.id, count(c)) " +
+            "FROM Comment c " +
+            "WHERE c.board.id in :boardIds " +
+            "GROUP BY c.board.id")
+    List<CommentCountDto> countByBoardIds(List<Long> boardIds);
+
+    List<Comment> findByBoardId(Long boardId);
+}
