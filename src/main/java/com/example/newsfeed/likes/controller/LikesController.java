bf7a693e823f7cf0f0a3c29426dfbe91ced9a17c
jyedev04 <jyedev04@gmail.com>
2025-02-18 17:59:22 +0900

    impl(likes) board 좋아요 수정 - LikesRepository.java 제거 - BoardLikesRepository 생성

--- src/main/java/com/example/newsfeed/likes/controller/LikesController.java
+++ src/main/java/com/example/newsfeed/likes/controller/LikesController.java
@@ -1,46 +1,36 @@
-//package com.example.newsfeed.likes.controller;
-//
-//import com.example.newsfeed.common.dto.ResponseDto;
-//import com.example.newsfeed.likes.service.BoardLikesService;
-//import com.example.newsfeed.likes.service.CommentLikesService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/api")
-//public class LikesController {
-//    private final CommentLikesService commentlikesService;
-//    private final BoardLikesService boardlikesService;
-//
-//    private AuthUsers getCurrentMemberInfo() {
-//        AuthUsers authUsers = (AuthUsers) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        return authUsers;
-//    }
-//
-//    @PostMapping("/comments/{commentId}/likes")
-//    public ResponseDto<?> addCommentLike(@PathVariable Long commentId){
-//        String userId = getCurrentMemberInfo().getUserId();
-//        return commentlikesService.addCommentLike(commentId, userId);
-//    }
-//
-//    @DeleteMapping("/comments/{commentId}/likes/{id}")
-//    public ResponseDto<?> cancelCommentLike(@PathVariable Long commentId, @PathVariable Long id){
-//        String userId = getCurrentMemberInfo().getUserId();
-//        return commentlikesService.cancelCommentLike(commentId, userId);
-//    }
-//
-//    @PostMapping("/boards/{boardId}/likes")
-//    public ResponseDto<?> addBoardLike(@PathVariable Long boardId){
-//        String userId = getCurrentMemberInfo().getUserId();
-//        return boardlikesService.addBoardLike(boardId, userId);
-//    }
-//
-//    @DeleteMapping("/comments/{boardId}/likes/{id}")
-//    public ResponseDto<?> cancelBoardLike(@PathVariable Long boardId, @PathVariable Long id){
-//        String userId = getCurrentMemberInfo().getUserId();
-//        return boardlikesService.cancelBoardLike(boardId, userId);
-//    }
-//
-//}
+package com.example.newsfeed.likes.controller;
+
+import com.example.newsfeed.common.dto.ResponseDto;
+import com.example.newsfeed.jwt.annotation.UserSession;
+import com.example.newsfeed.jwt.entity.AuthUsers;
+import com.example.newsfeed.likes.service.BoardLikesService;
+import com.example.newsfeed.likes.service.CommentLikesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/likes")
+public class LikesController {
+    private final CommentLikesService commentlikesService;
+    private final BoardLikesService boardlikesService;
+
+    @PostMapping("/{type}/{typeId}")
+    public ResponseDto<?> addBoardLike(@UserSession AuthUsers authUser, @PathVariable String type,  @PathVariable Long typeId){
+        return switch (type){
+            case "boards" -> boardlikesService.addBoardLike(typeId, authUser.getUserId());
+            case "comments" -> commentlikesService.addCommentLike(typeId, authUser.getUserId());
+            default-> throw new IllegalArgumentException("올바른 타입이 아닙니다.");
+        };
+    }
+
+    @DeleteMapping("/{type}/{typeId}")
+    public ResponseDto<?> cancelBoardLike(@UserSession AuthUsers authUser,  @PathVariable String type,  @PathVariable Long typeId) {
+        return switch (type){
+            case "boards" -> boardlikesService.cancelBoardLike(typeId, authUser.getUserId());
+            case "comments" -> commentlikesService.cancelCommentLike(typeId, authUser.getUserId());
+            default -> throw new IllegalArgumentException("올바른 타입이 아닙니다.");
+        };
+    }
+
+}
