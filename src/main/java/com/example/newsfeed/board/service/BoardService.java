f464093146c705b801137a40354a4d85f951174a
Seung(emily_the_dev) <emily101304@naver.com(emily101304)>
2025-02-18 17:27:37 +0900

    refactor : BoardController, BoardService 로그인된 user 받아올 수 있게 수정

--- src/main/java/com/example/newsfeed/board/service/BoardService.java
+++ src/main/java/com/example/newsfeed/board/service/BoardService.java
@@ -5,12 +5,17 @@ import com.example.newsfeed.board.dto.request.BoardUpdateRequestDto;
 import com.example.newsfeed.board.dto.response.BoardResponseDto;
 import com.example.newsfeed.board.entity.Board;
 import com.example.newsfeed.board.repository.BoardRepository;
+import com.example.newsfeed.common.FileUtil;
 import com.example.newsfeed.common.dto.ResponseDto;
+import com.example.newsfeed.jwt.entity.AuthUsers;
 import com.example.newsfeed.users.entity.Users;
 import com.example.newsfeed.users.repository.UsersRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 import java.util.List;
 
 
@@ -20,19 +25,35 @@ public class BoardService {
 
     private final BoardRepository boardRepository;
     private final UsersRepository usersRepository;
+    private final FileUtil fileUtil;
 
     @Transactional
-    public ResponseDto<BoardResponseDto> save(String userId, BoardSaveRequestDto dto) {
-        Users user = usersRepository.findById(userId).orElseThrow(
+    public ResponseDto<BoardResponseDto> save(
+            AuthUsers authUsers,
+            BoardSaveRequestDto dto,
+            MultipartFile file
+            ) {
+        Users user = usersRepository.findById(authUsers.getUserId()).orElseThrow(
                 () -> new IllegalArgumentException("해당 유저는 존재하지 않습니다.")
         );
+
         Board board = new Board(
                 user,
                 dto.getTitle(),
                 dto.getContent(),
-                dto.getImages(),
                 dto.getVisibilityType()
         );
+
+        String imagePath = "basicImage";
+        if (file != null && !file.isEmpty()) {
+            try {
+                // 파일 저장 및 경로 반환
+               imagePath = fileUtil.saveBoardImage(file, authUsers.getUserId(), board.getId());
+            } catch (IOException e) {
+                throw new IllegalStateException("파일 저장 중 오류가 발생했습니다.", e);
+            }
+        }
+        board.updateImage(imagePath);
         boardRepository.save(board);
 
         BoardResponseDto dtos = BoardResponseDto.toDto(board);
@@ -49,8 +70,8 @@ public class BoardService {
     }
 
     @Transactional
-    public ResponseDto<BoardResponseDto> findById(Long id) {
-        Board board = boardRepository.findById(id)
+    public ResponseDto<BoardResponseDto> findById(String boardId) {
+        Board board = boardRepository.findById(boardId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시물은 존재하지 않습니다."));
 
         BoardResponseDto dtos = BoardResponseDto.toDto(board);
@@ -58,11 +79,11 @@ public class BoardService {
     }
 
     @Transactional
-    public ResponseDto<BoardResponseDto> update(Long boardId, String userId, BoardUpdateRequestDto dto) {
+    public ResponseDto<BoardResponseDto> update(String boardId, AuthUsers authUsers, BoardUpdateRequestDto dto) {
         Board board = boardRepository.findById(boardId).orElseThrow(
                 () -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다.")
         );
-        if (!userId.equals(board.getUser().getId())) {
+        if (!authUsers.getUserId().equals(board.getUser().getId())) {
             throw new IllegalArgumentException("본인이 작성한 게시글만 수정할 수 있습니다.");
         }
         board.update(dto.getTitle(),dto.getContent(),dto.getImages(),dto.getVisibilityType());
@@ -70,13 +91,14 @@ public class BoardService {
     }
 
     @Transactional
-    public void deleteById(Long boardId, String userId) {
+    public void deleteById(String boardId, AuthUsers authUsers) {
         Board board = boardRepository.findById(boardId).orElseThrow(
                 ()-> new IllegalArgumentException("해당 게시물이 존재하지 않습니다.")
         );
-        if (!userId.equals(board.getUser().getId())) {
+        if (!authUsers.getUserId().equals(board.getUser().getId())) {
             throw new IllegalArgumentException("본인이 작성한 게시글만 삭제할 수 있습니다.");
         }
+        fileUtil.deleteBoardImage(authUsers.getUserId(), board.getId());
         boardRepository.delete(board);
     }
 }
