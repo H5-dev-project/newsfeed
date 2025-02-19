49cf423f2e6599749e6f1001cb9bfd7cbdf3cc02
myeonghoon <mhoo999@naver.com>
2025-02-19 22:03:03 +0900

    mapping 경로 수정 등

--- src/main/java/com/example/newsfeed/comment/controller/CommentController.java
+++ src/main/java/com/example/newsfeed/comment/controller/CommentController.java
@@ -7,6 +7,7 @@ import com.example.newsfeed.comment.service.CommentService;
 import com.example.newsfeed.common.dto.ResponseDto;
 import com.example.newsfeed.jwt.annotation.UserSession;
 import com.example.newsfeed.jwt.entity.AuthUsers;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,13 +15,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class CommentController {
 
     private final CommentService commentService;
 
     @PostMapping("/boards/{boardId}/comments")
-    public ResponseEntity<ResponseDto<CommentResponseDto>> save(@UserSession AuthUsers authUsers, @PathVariable String boardId, @RequestBody CommentSaveRequestDto dto) {
+    public ResponseEntity<ResponseDto<CommentResponseDto>> save(@Parameter(hidden = true) @UserSession AuthUsers authUsers, @PathVariable String boardId, @RequestBody CommentSaveRequestDto dto) {
         return ResponseEntity.ok(commentService.save(authUsers.getUserId(), boardId, dto));
     }
 
@@ -35,12 +37,12 @@ public class CommentController {
     }
 
     @PutMapping("/comments/{id}")
-    public ResponseEntity<ResponseDto<CommentResponseDto>> update(@UserSession AuthUsers authUsers, @PathVariable Long id, @RequestBody CommentUpdateRequestDto dto) {
+    public ResponseEntity<ResponseDto<CommentResponseDto>> update(@Parameter(hidden = true) @UserSession AuthUsers authUsers, @PathVariable Long id, @RequestBody CommentUpdateRequestDto dto) {
         return ResponseEntity.ok(commentService.update(id, authUsers.getUserId(), dto));
     }
 
     @DeleteMapping("/comments/{id}")
-    public ResponseEntity<Void> delete(@UserSession AuthUsers authUsers, @PathVariable Long id) {
+    public ResponseEntity<Void> delete(@Parameter(hidden = true) @UserSession AuthUsers authUsers, @PathVariable Long id) {
         commentService.delete(id, authUsers.getUserId());
         return ResponseEntity.ok().build();
     }
